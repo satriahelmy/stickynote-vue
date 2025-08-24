b4d311aa6bc511775b31d968c4d03a8e2398cff1
satriahelmy <helmysmp@gmail.com>
2025-08-24 19:16:59 +0700

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,51 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <div class="container">
+      <header>
+        <h1 class="header-title">Memo</h1>
+        <button class="header-button">+</button>
+      </header>
+      <div>[card]</div>
+    </div>
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+main{
+  height: 100vh;
+  width:100vw;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.container{
+  max-width: 900px;
+  padding: 10px;
+  margin:0 auto;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+header{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+.header-title{
+  font-size: 48px;
+  font-weight: bold;
+  margin-bottom: 25px;
+  color: #495a7d;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.header-button{
+  background-color: #495a7d;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  padding: 10px 15px;
+  cursor: pointer;
+}
+
+.header-button:hover{
+  background-color: #3b4a6b;
 }
 </style>
